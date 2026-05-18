12caf7a8eee4ab23275f1c9216128a007d49884b
Shayan <shayan.cms@gmail.com>
2026-05-18 23:52:14 +0500

    Firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAflA_-9Qk3pIUFI62enn0elnvo21qN4VE",
+  authDomain: "thyrocheck-bd5c4.firebaseapp.com",
+  projectId: "thyrocheck-bd5c4",
+  storageBucket: "thyrocheck-bd5c4.firebasestorage.app",
+  messagingSenderId: "115297128360",
+  appId: "1:115297128360:web:f8aa130ad6774dfe53179d"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+
+export default app;
